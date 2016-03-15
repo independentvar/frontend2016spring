e5ae5f5391ed742542da3a6c1bfc829b80c2fdc1
independentvar <lobinas.martynas@gmail.com>
2016-03-15 14:01:19 +0100

    Completed module and object excerises

--- lecture-6/module-exercise/js/script.js
+++ lecture-6/module-exercise/js/script.js
@@ -2,18 +2,42 @@
 	"use strict";
 
 	var modal = (function(){
-      
-      var init = function(){
-        console.log("Up and running");
-      }
-      
-      return {
-        init: init
-      }
+        var buttonOpen = document.querySelector(".modal-btn-open"),
+            buttonClose = document.querySelector(".modal-btn-close"),
+            cover = document.querySelector(".cover"),
+            modal = document.querySelector(".modal"),
+            hiddenClassName = "hidden";
+
+        buttonOpen.addEventListener("click", function(e){
+            e.preventDefault();
+
+            modal.classList.remove(hiddenClassName);
+            cover.classList.remove(hiddenClassName);
+        });
+
+        buttonClose.addEventListener("click", function(){
+            modal.classList.add(hiddenClassName);
+            cover.classList.add(hiddenClassName);
+        });
+
+        /*
+        function getModalName(classList){
+            for(var i = 0; classList.length; i++){
+                if()
+            }
+        }
+        */
+
+        var init = function(){
+            console.log("Up and running");
+        }
+
+        return {
+            init: init
+        }
       
     })();
   
     modal.init();
 
 })();
-
