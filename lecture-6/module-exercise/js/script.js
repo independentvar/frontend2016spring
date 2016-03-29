df7766231c54c456866fb74f9487c493d342735e
independentvar <martynas.lobinas@gmail.com>
2016-03-29 09:35:06 +0200

    Multiple modals windows.

--- lecture-6/module-exercise/js/script.js
+++ lecture-6/module-exercise/js/script.js
@@ -2,35 +2,39 @@
 	"use strict";
 
 	var modal = (function(){
-        var buttonOpen = document.querySelector(".modal-btn-open"),
-            buttonClose = document.querySelector(".modal-btn-close"),
+        var openButtons = document.getElementsByClassName("modal-btn-open"),
+            closeButtons = document.getElementsByClassName("modal-btn-close"),
             cover = document.querySelector(".cover"),
-            modal = document.querySelector(".modal"),
+            modals = document.getElementsByClassName("modal"),
+            currentModal = null,
             hiddenClassName = "hidden";
-
-        buttonOpen.addEventListener("click", function(e){
-            e.preventDefault();
-
-            modal.classList.remove(hiddenClassName);
-            cover.classList.remove(hiddenClassName);
-        });
-
-        buttonClose.addEventListener("click", function(){
-            modal.classList.add(hiddenClassName);
-            cover.classList.add(hiddenClassName);
-        });
-
-        /*
-        function getModalName(classList){
-            for(var i = 0; classList.length; i++){
-                if()
-            }
-        }
-        */
 
         var init = function(){
             console.log("Up and running");
-        }
+
+            for(var i = 0; i < openButtons.length; i++){
+                openButtons[i].addEventListener("click", openModal);
+            }
+
+            for(var i = 0; i < closeButtons.length; i++) {
+                closeButtons[i].addEventListener("click", closeModal);
+            }
+        };
+
+        var openModal = function(e){
+            e.preventDefault();
+            var hash = e.currentTarget.hash;
+            var id = hash.slice(1);
+            currentModal = document.getElementById(id);
+
+            currentModal.classList.remove(hiddenClassName);
+            cover.classList.remove(hiddenClassName);
+        };
+
+        var closeModal = function () {
+            currentModal.classList.add(hiddenClassName);
+            cover.classList.add(hiddenClassName);
+        };
 
         return {
             init: init
